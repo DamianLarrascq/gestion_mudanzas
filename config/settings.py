5692662c9d8c21bed1c5a331a5bf917d09651442
Solange <cruz.s.elizabeth@gmail.com>
2026-06-02 22:41:23 -0300

    Changes requirements for deploy

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default='localhost').split(',')
+ALLOWED_HOSTS = config("SCruz.pythonanywhere.com", default='localhost').split(',')
 
 
 # Application definition
@@ -202,3 +202,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
