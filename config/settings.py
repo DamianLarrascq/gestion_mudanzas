1c2213c70f5315cb1271c6fe92ab93cf756f2ec6
Solange <cruz.s.elizabeth@gmail.com>
2026-06-14 01:24:41 -0300

    Instalacion en requirements y cambios en tokens para tests

--- config/settings.py
+++ config/settings.py
@@ -155,9 +155,9 @@ CHATBOT_URL_FORMULARIO = ""
 CHATBOT_URL_WEB_PRINCIPAL = ""
 
 # Twilio / Whatsapp
-TWILIO_ACCOUNT_SID = config("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = config("TWILIO_AUTH_TOKEN")
-TWILIO_WHATSAPP_FROM = config("TWILIO_WHATSAPP_FROM")
+TWILIO_ACCOUNT_SID   = config("TWILIO_ACCOUNT_SID",   default="")
+TWILIO_AUTH_TOKEN    = config("TWILIO_AUTH_TOKEN",     default="")
+TWILIO_WHATSAPP_FROM = config("TWILIO_WHATSAPP_FROM",  default="")
 TWILIO_SANDBOX = config("TWILIO_SANDBOX", default=True, cast=bool)
 # URL pública de este servidor, usada para validar la firma de Twilio.
 # Debe coincidir exactamente con la URL configurada en el Twilio Console.
